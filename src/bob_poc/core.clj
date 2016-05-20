1a98c8d700fffa965080061639510f39ba19d004
Tommi Sarin <tommi.sarin@affecto.com>
2016-05-20 13:53:14 +0300

    Match loop implemented

--- src/bob_poc/core.clj
+++ src/bob_poc/core.clj
@@ -6,20 +6,20 @@
             [bob-poc.match.match-service :as match]
             [clj-time.core :as t]
             [clojure.core.async :refer :all])
-  (:import [java.lang Runtime Thread]))
+  (:import [java.lang Runtime Thread]
+           (org.joda.time Seconds)))
 
 (def ^:private app-server (atom nil))
 (def ^:private match-loop (atom nil))
 
 (def ^:private match-duration (t/seconds 15))
-
 (def ^:private between-match-wait (t/seconds 5))
 
 (defn- start-match-loop []
   (match/start-match! match-duration)
-  
+
   (let [stop (chan)
-        time-in-ms 20000]
+        time-in-ms (t/in-millis (.plus match-duration between-match-wait))]
     (go-loop []
        (alt!
          (timeout time-in-ms) (do (<! (thread (match/start-match! match-duration)))
@@ -27,13 +27,13 @@
          stop :stop))
     stop))
 
-(defn- start-server []
+(defn- start-server! []
   (info "Starting BoB-POC...")
   (reset! app-server (jetty/run-jetty app {:max-threads 100 :port 3399 :join? false}))
   (reset! match-loop (start-match-loop))
   (info "React test started successfully!"))
 
-(defn- stop-server []
+(defn- stop-server! []
   (info "Stopping BoB-POC...")
   (close! @match-loop)
   (.stop @app-server)
@@ -41,7 +41,7 @@
 
 (defn -main []
   (try
-    (start-server)
-    (.addShutdownHook (Runtime/getRuntime) (Thread. stop-server))
+    (start-server!)
+    (.addShutdownHook (Runtime/getRuntime) (Thread. stop-server!))
     (catch Exception e
       (do (error "Error happened in main" e) (throw e)))))
