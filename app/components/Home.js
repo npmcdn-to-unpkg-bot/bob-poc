169dc2b308310d807dd296953d96019c8799643e
Tommi Sarin <tommi.sarin@affecto.com>
2016-05-27 16:10:07 +0300

    Add node modules into project (amend)

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var MainContainer = require('../containers/MainContainer');
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<MainContainer>
+				<h1>Battle of Bands!</h1>
+			</MainContainer>
+		)
+	}
+});
+
+module.exports = Home;
