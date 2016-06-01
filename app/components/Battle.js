8911f7fa1c55697bb3d924181a7bfb569ae5f735
Tommi Sarin <tommi.sarin@affecto.com>
2016-06-01 12:35:53 +0300

    Get data from server to battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -12,20 +12,16 @@ function Battle (props) {
 		props.isLoading === true
 			? 	<Loading />
 			:	<MainWrapper>
-					<h1>Round 1</h1>
+					<h1>Round {props.match}</h1>
 					<div className='col-sm-8 col-sm-offset-2'>
-						<BandDetailsWrapper header="Band 1" votes={3}>
-							<BandDetails
-								band_id={1}
-								avatar_url="https://scontent.xx.fbcdn.net/t31.0-8/12697237_10153297954690264_675506397102150072_o.jpg"
-								soundcloud_url="https://soundcloud.com/redkaukasus/virago" />
-						</BandDetailsWrapper>
-						<BandDetailsWrapper header="Band 2" votes={6}>
-							<BandDetails
-								band_id={2}
-								avatar_url="https://scontent.xx.fbcdn.net/t31.0-8/12697237_10153297954690264_675506397102150072_o.jpg"
-								soundcloud_url="https://soundcloud.com/redkaukasus/veil-kite" />
-						</BandDetailsWrapper>
+						{props.standoff.map(function (band) {
+							return (<BandDetailsWrapper header={band.name} votes={band.votes} key={band.name + '-' + props.match + '-' + band.votes}>
+										<BandDetails
+											band_id={band.id}
+											avatar_url="https://scontent.xx.fbcdn.net/t31.0-8/12697237_10153297954690264_675506397102150072_o.jpg"
+											soundcloud_url="https://soundcloud.com/redkaukasus/virago" />
+									</BandDetailsWrapper>)
+						})}
 					</div>
 				</MainWrapper>
 	)
--- app/containers/BattleContainer.js
+++ app/containers/BattleContainer.js
@@ -5,20 +5,25 @@ var serverConnector = require('../server/serverConnector');
 var BattleContainer = React.createClass({
 	getInitialState: function () {
 		return {
-			isLoading: true
+			isLoading: true,
+			match: 0,
+			standoff: []
 		}
 	},
 	componentDidMount: function () {
 		serverConnector.getBattleInfo()
 			.then(function (battle) {
+				console.log("BATTLE", battle);
 				this.setState({
-					isLoading: false
+					isLoading: false,
+					match: battle.data.match,
+					standoff: battle.data.standoff
 				})
 			}.bind(this))
 	},
 	render: function () {
 		return (
-			<Battle isLoading={this.state.isLoading}/>
+			<Battle isLoading={this.state.isLoading} match={this.state.match} standoff={this.state.standoff} />
 		)
 	}
 });
