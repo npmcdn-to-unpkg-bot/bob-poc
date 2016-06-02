db32b28e22377bdb16d572c3d03c623862eced57
Tommi Sarin <tommi.sarin@affecto.com>
2016-06-02 16:23:34 +0300

    Fixing heroku config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 		'./app/index.js'
 	],
 	output: {
-		path: __dirname + '/resources/public/scripts',
+		path: __dirname + '/resources/public',
 		filename: "index_bundle.js"
 	},
 	module: {
