3e1113c0f13eae808f307e937b255df657c47d3e
Tommi Sarin <tommi.sarin@affecto.com>
2016-06-07 12:52:53 +0300

    Modified soundcloud player a bit

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,10 +1,18 @@
 var React = require('react');
+var SC = require('soundcloud');
 
 var Player = React.createClass({
     componentDidMount: function() {
+        SC.initialize({
+            client_id: 'f372d41f0b8cd51c53240982350ab4fb'
+        });
         SC.oEmbed(this.props.soundcloudUrl, {
-            maxHeight: "100",
-            element: document.getElementById('soundCloudDiv' + this.props.bandId)
+            element: document.getElementById('soundCloudDiv' + this.props.bandId),
+            maxheight: 150,
+            buying: false,
+            sharing: false,
+            show_user: false,
+            show_artwork: true
         });
     },
     render: function () {
